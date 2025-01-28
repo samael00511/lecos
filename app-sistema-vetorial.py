5ec78aa9f350c48347ddbbc414466101cd6a359d
samael00511 <samael.rocha@alu.ufc.br>
2025-01-28 20:56:49 -0300

    Update app-sistema-vetorial.py

--- app-sistema-vetorial.py
+++ app-sistema-vetorial.py
@@ -181,7 +181,7 @@ app.layout = html.Div(
                 html.Div(
                     style={
                         'width': '100%',  # Largura maior para acomodar os gráficos
-                        'height': '900px',
+                        'height': '100%', # 900px
                         'flexDirection': 'column', # row
                         'backgroundColor': '#457b9d',
                         'margin-left': '10px',
